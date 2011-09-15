95e53051f5e76f6794b492e079e82fd924eb67ea
bilalnazir <bilalnazeer@live.com>
2011-09-15 23:14:27 +0500

    added prompt for name and say hello back

--- console_app/Program.cs
+++ console_app/Program.cs
@@ -12,7 +12,7 @@ namespace console_app
             Console.WriteLine("What's you name?");
             var name = Console.ReadLine();
             Console.WriteLine("Hello, " + name.ToString());
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
     }
 }
